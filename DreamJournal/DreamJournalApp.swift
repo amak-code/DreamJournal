6f83dac63e763e62bc69fef25e7deed0411c39c6
Aleksandra Makhrova <amakhro@gmail.com>
2022-07-06 09:41:53 -0700

    Initial Commit

---
+++ DreamJournal/DreamJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamJournalApp.swift
+//  DreamJournal
+//
+//  Created by antikiller on 06.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct DreamJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
